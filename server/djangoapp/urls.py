379a5c278b6dc43e96824a18cf29eb91b72d670f
Francisco Lemus <fjmorataya1@gmail.com>
2026-03-24 19:06:14 -0400

    Avances en modulo 2 para agregar nuevas funciones a front end

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
 from . import views
 
 app_name = 'djangoapp'
@@ -10,6 +11,9 @@ urlpatterns = [
 
     # path for login
     path(route='login', view=views.login_user, name='login'),
+    path(route='logout', view=views.logout_request, name='logout'),
+    path(route='register', view=views.registration, name='register'),
+    path('login/', TemplateView.as_view(template_name="index.html")),
 
     # path for dealer reviews view
 
