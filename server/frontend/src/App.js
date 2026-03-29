ddf27114fd736566c6152166ec75277e962a5832
Francisco Lemus <fjmorataya1@gmail.com>
2026-03-29 16:02:07 -0400

    Solucionado problema con la ruta registro

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,12 +1,16 @@
 import LoginPanel from "./components/Login/Login"
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import RegisterPanel from "./components/Register/Register";
 
 function App() {
+
+    const location = useLocation();
+    console.log("Ruta actual: ", location.pathname);
+
   return (
     <Routes>
-      <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<RegisterPanel />} />
+      <Route path="/login" element={<LoginPanel />} />
     </Routes>
   );
 }
