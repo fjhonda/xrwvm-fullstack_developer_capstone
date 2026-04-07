3ed50e76129fdb95f3ecd9b52df2a8c0bcccb18d
Francisco Lemus <fjmorataya1@gmail.com>
2026-04-07 19:46:26 -0400

    agregados nuevos componentes para las paginas dinamicas

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,6 +1,9 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route, useLocation } from "react-router-dom";
 import RegisterPanel from "./components/Register/Register";
+import Dealers from './components/Dealers/Dealers';
+import Dealer from "./components/Dealers/Dealer"
+
 
 function App() {
 
@@ -11,6 +14,9 @@ function App() {
     <Routes>
       <Route path="/register" element={<RegisterPanel />} />
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/dealers" element={<Dealers/>} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
+
     </Routes>
   );
 }
