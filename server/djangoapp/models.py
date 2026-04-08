c95ae80b6bffa5fb202b9187a26010b452f41cc7
Francisco Lemus <fjmorataya1@gmail.com>
2026-04-07 20:33:08 -0400

    Corregido linting

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,6 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -13,7 +12,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
 class CarMake(models.Model):
-    name= models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     description = models.TextField()
 
     def __str__(self):
