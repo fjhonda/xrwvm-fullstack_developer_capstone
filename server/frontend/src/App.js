e674e93ffd46b4a26b4255c8f9a0007b8dbfc971
Francisco Lemus <fjmorataya1@gmail.com>
2026-04-07 20:07:12 -0400

    Agregada ruta para hacer un post de nuevos reviews

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import RegisterPanel from "./components/Register/Register";
 import Dealers from './components/Dealers/Dealers';
 import Dealer from "./components/Dealers/Dealer"
+import PostReview from "./components/Dealers/PostReview"
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/dealers" element={<Dealers/>} />
       <Route path="/dealer/:id" element={<Dealer/>} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
 
     </Routes>
   );
